a398f65c1594b40ef6c95134e0fcf4a27ac04683
Delzad Bamji <delzadbamji@gmail.com>
2021-06-14 02:51:45 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '9f99d8b6a82d44f8b900d6d771d55171'
+ apiKey: env.API_KEY
 });
 
 const particlesOptions = {
@@ -81,19 +81,12 @@ class App extends Component {
     this.setState({imageUrl: this.state.input});
     app.models
       .predict(
-        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-        // so you would change from:
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        // to:
-        // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+        // If the Clarifai Models are down or not working, check if the model is still available on clarifai 
+        //(https://www.clarifai.com/models/face-detection)
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
       .then(response => {
-        console.log('hi', response)
+        console.log('check response: ', response)
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
